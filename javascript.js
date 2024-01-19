f6356b7f0b96d8fc0f4e88e828f68569acb66226
Vincent <2w121q@gmail.com>
2024-01-19 10:46:36 +0800

    Make the calculator work

--- javascript.js
+++ javascript.js
@@ -32,14 +32,63 @@ function operate(operator, x, y) {
     }
 }
 
+function roundOff(n) {
+    if (n.toString().includes('.')) {
+        let noDec = n.toString().split('.')[1].length;
+        if (noDec > 4) {
+            n = n.toFixed(2);
+            return n;
+        }
+    }
+    
+    return n;
+}
+
+let clear, reset;
 let display = document.querySelector('#display');
 let buttons = document.querySelector('#buttons');
 buttons.addEventListener('click', function(e) {
     let button = e.target;
 
     if (button.textContent == 0 && display.textContent == 0);
-    else if (button.textContent == 'C') display.textContent = 0;    
-    else if (display.textContent == 0) display.textContent = button.textContent;    
-    else display.textContent += button.textContent;
-    
+    else if (button.textContent == '+' || button.textContent == '-' || button.textContent == '*' || button.textContent == '/') {
+        if (x && reset != true) {
+            y = display.textContent;
+            x = roundOff(operate(operator, +x, +y));
+            display.textContent = x;
+            operator = button.textContent;            
+        }
+        else {
+            operator = button.textContent;
+            x = display.textContent;
+        }
+        clear = true;
+    }
+    else if (button.textContent == '=') {
+        if (operator) {
+            y = display.textContent;
+            if (operator == '/' && y == 0) {
+                display.textContent = 'ERROR';
+                clear = true;
+            }
+            else {
+                display.textContent = roundOff(operate(operator, +x, +y));                
+            }
+            reset = true;
+        }
+        else ;
+        
+    }
+    else if (button.textContent == 'C') {
+        display.textContent = 0;
+        x = null;
+        y = null;
+        operator = null;
+    }
+    else if (display.textContent == 0 || clear) {
+        display.textContent = button.textContent;
+        clear = false;
+    }
+    else display.textContent += button.textContent;    
+
 });
